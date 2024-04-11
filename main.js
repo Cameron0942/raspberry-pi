3924f60fb4500e51c18cfef1fd066e846e027291
Cameron <28991785+Cameron0942@users.noreply.github.com>
2024-04-11 00:17:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+const jsautogui = require('jsautogui');
 console.log('hello I am a working javascript file. hows it goin');
+
+async function moveMouse() {
+    try {
+        await jsautogui.moveMouse(100, 100);
+        console.log('Mouse moved to position (100, 100).');
+    } catch (error) {
+        console.error('Failed to move the mouse:', error);
+    }
+}
+
+moveMouse();
