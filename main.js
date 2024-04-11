2cd1ce88e102ab0673a30e42c4fa56e8c62be6e9
Cameron B <Cameron0942@users.noreply.github.com>
2024-04-11 00:45:55 -0700

    add all files

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const {mouse} = require("jsautogui");
 console.log('hello I am a working javascript file. hows it goin');
 
-mouse.moveTo(100, 200, 5000); // duration is in milliseconds, 5000ms is the equavalent of 5 seconds.
+// mouse.moveTo(100, 200, 5000); // duration is in milliseconds, 5000ms is the equavalent of 5 seconds.
+mouse.position = { x: 300, y: 400 };
+mouse.moveTo(100, 200, 5000)
