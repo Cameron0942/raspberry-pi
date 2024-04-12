7075e6406f2d697aacc5911955149684cdb9ecd8
Cameron <28991785+Cameron0942@users.noreply.github.com>
2024-04-11 21:56:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# import pyautogui as pg
-import requests
-import time
-import json
-import mysql.connector
-from datetime import datetime
-import random
+import pyautogui as pg
+# import requests
+# import time
+# import json
+# import mysql.connector
+# from datetime import datetime
+# import random
 
 print('hello I am a working python file, I hope this works')
+pg.moveTo(500, 200, 5)
